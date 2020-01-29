ab606f51f7753ea628bde058a0c0f482abbed03c
Drake Wilcox <drakewilcox@gmail.com>
2020-01-28 21:44:52 -0800

    add changes to scripts so that the ticket price varied depending on user input and displayed as a result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,54 @@
 /// Business Logic for Output
 
-function TicketList() {
-  this.tickets = [];
-  this.ticketId = 0;
-  this.totalPrice = 0;
-}
-
-TicketList.prototype.addTicket = function(ticket) {
-  ticket.id = this.assignId();
-  
-  this.tickets.push(ticket);
-}
-
-
-// MovieTicket.prototype.ticketPrice = function(ticket){
-//   if var inputtedAge === 
-// }
-
-
-
-
-// MovieTicket.prototype.ticketPrice = function(ticket){
-//   for (var i=0; i< this.tickets.length; i++)
-//     if (this.tickets[i]){
-//       if (this.tickets[i].film === "feature"){
-        
-//       }
-  
-//     }
-// };
-
-
-TicketList.prototype.findPrice = function() {
-  if (ticket.film == "feature") { 
-  ticket.price == 6;
-  }
-}
-TicketList.prototype.assignId = function(){
-  this.ticketId +=1;
-  return this.ticketId;
-}
-TicketList.prototype.findTicket = function(id) {
-  for (var i=0; i< this.tickets.length; i++) {
-    if (this.tickets[i]) {
-      if (this.tickets[i].id == id) {
-        return this.tickets[i];
-      }
-    }
-  };
-  return false;
-}
 
 // business logic for Ticket
 
-function Ticket(film, time, age, price) {
+function MovieTicket(film, time, age) {
+  this.normalPrice = 10
   this.film = film,
   this.time = time,
-  this.age = age,
-  this.price = price;
- 
+  this.age = age;
 }
+MovieTicket.prototype.update = function(name,time,age)  {
+  this.name = name;
+  this.time = time;
+  this.age = age;
+};
+MovieTicket.prototype.getPrice = function() {
+  var discount = 0 
+  if (this.age === "minor")  {
+    discount += 5;
+  } else if (this.age === "senior"){
+    discount += 1;
+  }
+  if(this.time === "matinee"){
+    discount += 1
+  }
+  if(this.film === "rerun") {
+    discount += 2
+  }
+  if(this.film === "classic") {
+    discount += 3
+  }
+  return this.normalPrice - discount;
+};
 
+function makeTicket() {
+  var film = $("#film").val();
+  var time = $("#time").val();
+  var age = $("#age").val();
+  ticket.update(film, time, age);
+
+  var ticketPrice = ticket.getPrice();
+  $("#output").text('Your Ticket Price: $' + ticketPrice + '.00');
+  $("#box").show();
+}
 // USER INTERFACE LOGIC:
-
-var ticketList = new TicketList();
+var ticket = new MovieTicket();
 
 $(document).ready(function()  {
   $("form#movies").submit(function(event) {
     event.preventDefault();
-    var inputtedFilm = $("#film").val();
-    var inputtedTime = $("#time").val();
-    var inputtedAge = $("#age").val();
-
-    $("#film").val();
-    $("#time").val();
-    $("#age").val();
-
-var newTicket = new Ticket(inputtedFilm, inputtedTime, inputtedAge);
-ticketList.addTicket(newTicket);
-tickets.findTicket(newTicket);
-// displayTicketDetails(movieTicket);
+    makeTicket();
   });
 });
